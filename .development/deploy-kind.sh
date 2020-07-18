3ef986e43aba9326d772baf3daf69630914f2d40
Alexander Krause <akr@informatik.uni-kiel.de>
2020-07-18 11:03:54 +0200

    use specific kind-cluster

--- .development/deploy-kind.sh
+++ .development/deploy-kind.sh
@@ -4,6 +4,6 @@
 
 docker build -f src/main/docker/Dockerfile.jvm -t explorviz/landscape-service-jvm .
 
-kind load docker-image explorviz/landscape-service-jvm:latest
+kind load docker-image --name explorviz-dev explorviz/landscape-service-jvm:latest
 
-kubectl apply --namespace=explorviz-dev -f manifest.yml
+kubectl apply --context kind-explorviz-dev -f manifest.yml
