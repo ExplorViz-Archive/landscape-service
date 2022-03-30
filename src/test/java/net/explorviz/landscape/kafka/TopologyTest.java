c01fc84d3bf9bd6c20e73f68ba56fcc335267d60
Alexander Krause <akr@informatik.uni-kiel.de>
2022-03-30 12:28:39 +0200

    TopologyTest with mocked cache and cassandra

--- src/test/java/net/explorviz/landscape/kafka/TopologyTest.java
+++ src/test/java/net/explorviz/landscape/kafka/TopologyTest.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import io.quarkus.test.junit.QuarkusTest;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import javax.inject.Inject;
 import net.explorviz.avro.SpanStructure;
 import net.explorviz.avro.Timestamp;
+import net.explorviz.landscape.persistence.SpanStructureRepositoy;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.TestInputTopic;
@@ -18,10 +21,14 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @QuarkusTest
 class TopologyTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TopologyTest.class);
 
   private TopologyTestDriver driver;
 
@@ -38,10 +45,12 @@ class TopologyTest {
 
   @Inject
   SpanCache spanCache;
+  
+  @Inject
+  SpanStructureRepositoy repository;
 
   @BeforeEach
   void setUp() {
-
     final Properties config = new Properties();
     config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
     config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class.getName());
@@ -75,13 +84,19 @@ class TopologyTest {
   }
 
   @Test
-  void testCache() {
+  void testCache() throws InterruptedException, ExecutionException {
     final SpanStructure testSpan = this.sampleSpanStructure();
-
+    
+    List<net.explorviz.landscape.persistence.model.SpanStructure> spanStructuresWithToken = this.repository.getAll(testSpan.getLandscapeToken()).collect().asList().subscribeAsCompletionStage().get();
+    
+    assertTrue(spanStructuresWithToken.size() == 0);
     assertFalse(this.spanCache.exists(testSpan.getHashCode()));
 
     this.inputTopic.pipeInput(testSpan.getLandscapeToken(), testSpan);
+    
+    spanStructuresWithToken = this.repository.getAll(testSpan.getLandscapeToken()).collect().asList().subscribeAsCompletionStage().get();
 
+    assertTrue(spanStructuresWithToken.size() == 1);
     assertTrue(this.spanCache.exists(testSpan.getHashCode()));
   }
 
