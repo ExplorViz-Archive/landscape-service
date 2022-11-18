b984f0a7ab461406ad66b279c235199b71aec82a
Alexander Krause <akr@informatik.uni-kiel.de>
2022-11-18 11:25:23 +0100

    use lazy init and comeback when cassandra is down

--- src/main/java/net/explorviz/landscape/persistence/cassandra/dao/SpanStructureDaoProducer.java
+++ src/main/java/net/explorviz/landscape/persistence/cassandra/dao/SpanStructureDaoProducer.java
@@ -1,6 +1,8 @@
 package net.explorviz.landscape.persistence.cassandra.dao;
 
 import com.datastax.oss.quarkus.runtime.api.session.QuarkusCqlSession;
+import io.smallrye.mutiny.Uni;
+import java.time.Duration;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Produces;
@@ -11,11 +13,14 @@ import javax.ws.rs.Produces;
 @ApplicationScoped
 public class SpanStructureDaoProducer {
 
+  private static final long CQL_TIMEOUT_SECONDS = 5;
+
   private final ReactiveSpanStructureDao reactiveSpanStructureDao;
 
   @Inject
-  public SpanStructureDaoProducer(final QuarkusCqlSession session) {
-    final SpanStructureMapper mapper = new SpanStructureMapperBuilder(session).build();
+  public SpanStructureDaoProducer(final Uni<QuarkusCqlSession> session) {
+    final SpanStructureMapper mapper = new SpanStructureMapperBuilder(session.await().atMost(
+        Duration.ofSeconds(CQL_TIMEOUT_SECONDS))).build();
     this.reactiveSpanStructureDao = mapper.reactiveSpanStructureDao();
   }
 
