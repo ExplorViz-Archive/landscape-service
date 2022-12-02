9ebfea6eccf02247cd474bcbbb4fb3b321ee081f
Alexander Krause <akr@informatik.uni-kiel.de>
2022-12-02 07:38:26 +0100

    rename mock service

--- src/test/java/net/explorviz/landscape/kafka/MockReactiveSpanStructureService.java
+++ src/test/java/net/explorviz/landscape/kafka/MockReactiveSpanStructureService.java
@@ -1,6 +1,5 @@
 package net.explorviz.landscape.kafka;
 
-import com.google.errorprone.annotations.ForOverride;
 import io.quarkus.test.Mock;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
@@ -9,12 +8,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.enterprise.context.ApplicationScoped;
 import net.explorviz.landscape.persistence.model.SpanStructure;
-import net.explorviz.landscape.service.cassandra.ReactiveLandscapeService;
 import net.explorviz.landscape.service.cassandra.ReactiveSpanStructureService;
 
 @Mock
 @ApplicationScoped
-public class MockSpanStructureRepositoryImpl extends ReactiveSpanStructureService {
+public class MockReactiveSpanStructureService extends ReactiveSpanStructureService {
 
   private final List<SpanStructure> spanStructures = new ArrayList<>();
 
